775130a43ba7bcac809c3b4aeda577a18793c516
tom_glb <86527957glb@qq.com>
2020-05-20 20:56:22 +0800

    第二次提交

--- src/main/java/com/archer/MainTest.java
+++ src/main/java/com/archer/MainTest.java
@@ -9,6 +9,8 @@ public class MainTest {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
         Person bean = applicationContext.getBean(Person.class);
+
+
         System.out.println(bean);
     }
 }
